b682d26afba785c8eafa0379828caf3efba107ad
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-27 19:17:27 -0300

    Update equipos.js

--- equipos.js
+++ equipos.js
@@ -2,45 +2,18 @@
    EQUIPOS - NOVA CLASH 2026
 ========================================== */
 
-export const grupos = {
+export const octavos = {
 
-A: [
-"Team Yetis",
-"Black Cat",
-"CPH Brazinos",
-"The Thunders",
-"The Gods Of War",
-"Shirin Ryu Gaming"
-],
+["Black Cat","Heirs Of J.P"],
+["The Thunders","Gigachad"],
+["Shirin Ryu Gaming","Los Squidibi Sigma"],
+["Team Yetis","Team Cheddar"],
+["Jema Chicer","Ultra Chmicha"],
+["Los Fracasados","TPE"],
+["Dark Domain","Community 8"],
+["Tatakae","Los Tilines De Guadalajara"]
 
-B: [
-"Heirs Of J.P",
-"Team Cheddar",
-"Cat Ezports",
-"Gigachad",
-"Los Campeones",
-"The Squidibi Sigma"
-],
-
-C: [
-"Jema Chicer",
-"MJX",
-"Los Fracasados",
-"Tatakae",
-"Dark Domain",
-"SX"
-],
-
-D: [
-"Los Indestructibles",
-"Ultra Chmicha",
-"Tilines de Guadalajara",
-"TPE",
-"Community 8",
-"Krew Rabels"
-]
-
-};
+];
 
 /* ==========================================
    NOMBRES DE LOS GRUPOS
