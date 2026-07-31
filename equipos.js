f5643d4bd6fcf3e7d9de665e952d45a66fb17bad
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-31 20:08:15 -0300

    Replace octavos with cuartos in equipos.js
    Updated tournament structure by replacing octavos with cuartos.

--- equipos.js
+++ equipos.js
@@ -1,10 +1,5 @@
-export const octavos = [
-  ["Black Cat", "Heirs Of J.P"],
-  ["The Thunders", "Gigachad"],
-  ["Shirin Ryu Gaming", "Los Squidibi Sigma"],
-  ["Team Yetis", "Team Cheddar"],
-  ["Jema Chicer", "Ultra Chmicha"],
-  ["Los Fracasados", "TPE"],
-  ["Dark Domain", "Community 8"],
-  ["Tatakae", "Los Tilines De Guadalajara"]
+export const cuartos = [
+  ["Black Cat", "Gigachad"],
+  ["Shirin Ryu Gaming", "Team Yetis"],
+  ["Jema Chicer", "TPE"],
 ];
