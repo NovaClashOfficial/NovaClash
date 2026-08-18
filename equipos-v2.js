e15c70e00a0ca52785df718dcb7f4a65283e00da
NovaClashOfficial <alexgamerproyt577@gmail.com>
2026-08-18 16:29:18 -0300

    Add equiposV2 data structure for teams

---
+++ equipos-v2.js
@@ -0,0 +1,61 @@
+console.log("EQUIPOS NOVA CLASH V2");
+
+/* ==========================================
+   EQUIPOS
+========================================== */
+
+export const equiposV2 = [
+
+  {
+    id: "equipo-01",
+    nombre: "Equipo 1",
+
+    jugadores: [
+      "Jugador 1",
+      "Jugador 2",
+      "Jugador 3"
+    ],
+
+    grupo: "A"
+  },
+
+  {
+    id: "equipo-02",
+    nombre: "Equipo 2",
+
+    jugadores: [
+      "Jugador 1",
+      "Jugador 2",
+      "Jugador 3"
+    ],
+
+    grupo: "A"
+  },
+
+  {
+    id: "equipo-03",
+    nombre: "Equipo 3",
+
+    jugadores: [
+      "Jugador 1",
+      "Jugador 2",
+      "Jugador 3"
+    ],
+
+    grupo: "A"
+  },
+
+  {
+    id: "equipo-04",
+    nombre: "Equipo 4",
+
+    jugadores: [
+      "Jugador 1",
+      "Jugador 2",
+      "Jugador 3"
+    ],
+
+    grupo: "A"
+  ]
+
+];
